ce8d8b61fc6bdcc45d91d6fe4a5da0ea93d36a01
chengyaoZhang <tc2002618@126.com>
2018-08-23 16:39:12 +0800

    solution to computing scc

--- src/Solution.java
+++ src/Solution.java
@@ -1,26 +1,27 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Stack;
+import java.util.*;
 
 public class Solution {
     static class Graph{
-        ArrayList<ArrayList<Integer>> edges;
+        ArrayList<ArrayList<Integer>> original_edges;
         ArrayList<ArrayList<Integer>> reversed_edges;
+        ArrayList<ArrayList<Integer>> modified_edges;
         ArrayList<Boolean> status;
         int[] finishing_time;
         int[] leader;
         int t = 0;
-        int leader = -1;
 
         public Graph(String filename, int num_vertices){
-            edges = new ArrayList<>(num_vertices);
+            original_edges = new ArrayList<>(num_vertices);
             reversed_edges = new ArrayList<>(num_vertices);
+            modified_edges = new ArrayList<>(num_vertices);
             status = new ArrayList<>(num_vertices);
             for(int i=0;i<num_vertices;i++){
-                edges.add(new ArrayList<>());
+                original_edges.add(new ArrayList<>());
                 reversed_edges.add(new ArrayList<>());
+                modified_edges.add(new ArrayList<>());
                 status.add(false);
             }
             finishing_time = new int[num_vertices];
@@ -36,11 +37,64 @@ public class Solution {
                     String[] items = line.trim().split(" ");
                     from = Integer.parseInt(items[0]);
                     to = Integer.parseInt(items[1]);
-                    edges.get(from-1).add(to);
+                    original_edges.get(from-1).add(to);
                     reversed_edges.get(to-1).add(from);
                 }
             }catch(IOException e){
                 e.printStackTrace();
+            }
+        }
+
+        public void computeSCCs(){
+            DFS_Loop(true);
+            modifyGraph();
+            for(int i=0;i<status.size();i++){
+                status.set(i, false);
+            }
+            DFS_Loop(false);
+
+            HashMap<Integer, Integer> count = new HashMap<>();
+            for(int i=0;i<leader.length;i++){
+                if(!count.containsKey(leader[i])){
+                    count.put(leader[i], 1);
+                }else{
+                    count.put(leader[i], count.get(leader[i])+1);
+                }
+            }
+            Collection<Integer> sccs = count.values();
+            int[] res = new int[5];
+            for(Integer item:sccs){
+                int i = 0;
+                while(i<5 && item<res[i]) i++;
+                if(i<5){
+                    for(int j=4;j>i;j--){
+                        res[j] = res[j-1];
+                    }
+                    res[i] = item;
+                }
+            }
+            for(int i=0;i<5;i++){
+                System.out.println(res[i]);
+            }
+        }
+
+        private void modifyGraph(){
+            // replace original labels with finishing time
+            for(int i=0;i<finishing_time.length;i++){
+                modified_edges.set(finishing_time[i]-1, original_edges.get(i));
+            }
+            for(ArrayList<Integer> v:modified_edges){
+                for(int i=0;i<v.size();i++){
+                    v.set(i, finishing_time[v.get(i)-1]);
+                }
+            }
+        }
+
+        private void DFS_Loop(boolean reverse){
+            for(int i = original_edges.size(); i>0; i--){
+                if(!status.get(i-1)){
+                    DFS(i, reverse);
+                }
             }
         }
 
@@ -50,9 +104,10 @@ public class Solution {
             while(!stack.isEmpty()){
                 int cur = stack.peek();
                 status.set(cur-1, true);
+                leader[cur-1] = startNode;
                 ArrayList<Integer> adjs;
                 if(!reverse){
-                    adjs = edges.get(cur-1);
+                    adjs = modified_edges.get(cur-1);
                 }else{
                     adjs = reversed_edges.get(cur-1);
                 }
@@ -63,18 +118,17 @@ public class Solution {
                 }
                 if(cur==stack.peek()){
                     stack.pop();
+                    if(finishing_time[cur-1]==0){
+                        t++;
+                        finishing_time[cur-1] = t;
+                    }
                 }
             }
-            status.set(startNode-1, true);
-
         }
     }
 
     public static void main(String[] args){
         Graph graph = new Graph("SCC.txt", 875714);
-        ArrayList<Integer> v = graph.edges.get(2);
-        for(Integer item:v){
-            System.out.print(item+" ");
-        }
+        graph.computeSCCs();
     }
 }
