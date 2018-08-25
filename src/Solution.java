8b13b8efb3ec31fd1f8cbd44e682b0b3c915a785
chengyaoZhang <tc2002618@126.com>
2018-08-25 17:43:45 +0800

    Solution to Dijkstra's shortest path algorithm

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
@@ -30,7 +29,7 @@ public class Solution {
                 BufferedReader br = new BufferedReader(new FileReader(filename));
                 String line;
                 while((line=br.readLine())!=null){
-                    String[] items = line.split("\t");
+                    String[] items = line.trim().split("\t");
                     int from = Integer.parseInt(items[0]);
                     int to;
                     int weight;
@@ -49,16 +48,35 @@ public class Solution {
             edges.add(new Edge(from, to, weight));
         }
 
-        public int[] Dijkstra(){
-
+        public int[] Dijkstra(int startingVertex){
+            HashSet<Integer> X = new HashSet<>();
+            X.add(startingVertex);
+            for(int i=1;i<shortestPath.length;i++){
+                int min = 1000000;
+                int cur = 0;
+                for(Edge e:edges){
+                    if(X.contains(e.tail) && !X.contains(e.head)){
+                        // crossing edges
+                        int dijkstra_greedy_score = shortestPath[e.tail-1] + e.weight;
+                        if(dijkstra_greedy_score<min){
+                            shortestPath[e.head-1] = dijkstra_greedy_score;
+                            min = dijkstra_greedy_score;
+                            cur = e.head;
+                        }
+                    }
+                }
+                X.add(cur);
+            }
+            return shortestPath;
         }
     }
 
     public static void main(String[] args){
-        Graph graph = new Graph("dijkstraData.txt", 200);
-        for(int i=0;i<12;i++){
-            Edge edge = graph.edges.get(i);
-            System.out.println(edge.tail+" --> "+edge.head+" Weight = "+edge.weight);
+        Graph graph = new Graph("dijkstraData.txt",200);
+        int[] targets = {7,37,59,82,99,115,133,165,188,197};
+        int[] shortestPath = graph.Dijkstra(1);
+        for(int i=0;i<targets.length;i++){
+            System.out.print(shortestPath[targets[i]-1]+",");
         }
     }
 }
