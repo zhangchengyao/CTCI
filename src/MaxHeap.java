737ca93499fadbd03ec0760bc79079c2c70db39b
chengyaoZhang <tc2002618@126.com>
2018-08-28 21:08:32 +0800

    median maintenance

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -1,11 +1,11 @@
-public class MinHeap {
-    private int[] data;
-    private int max_num;
-    private int num;
+public class MaxHeap {
+    int[] data;
+    int max_num;
+    int num;
 
-    public MinHeap(int max_num){
-        this.max_num = max_num;
+    public MaxHeap(int max_num){
         data = new int[max_num];
+        this.max_num = max_num;
         num = 0;
     }
 
@@ -15,15 +15,15 @@ public class MinHeap {
         bubble_up(num-1);
     }
 
-    public int extract_min(){
-        int min = data[0];
+    public int extract_max(){
+        int max = data[0];
         data[0] = data[num-1];
         num--;
         bubble_down(0);
-        return min;
+        return max;
     }
 
-    public int get_min(){
+    public int get_max(){
         return data[0];
     }
 
@@ -34,7 +34,7 @@ public class MinHeap {
     private void bubble_up(int start){
         int tmp = data[start];
         int cur = start;
-        while(cur>0 && tmp<data[(cur-1)/2]){
+        while(cur>0 && tmp>data[(cur-1)/2]){
             data[cur] = data[(cur-1)/2];
             cur = (cur-1)/2;
         }
@@ -46,10 +46,10 @@ public class MinHeap {
         int cur = start;
         while(cur*2+1<num){
             int child = cur*2+1;
-            if(child+1<num && data[child+1]<data[child]){
+            if(child+1<num && data[child+1]>data[child]){
                 child++;
             }
-            if(tmp>data[child]){
+            if(tmp<data[child]){
                 data[cur] = data[child];
                 cur = child;
             }else{
--- src/Solution.java
+++ src/Solution.java
@@ -1,103 +1,46 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
 
 public class Solution {
-    static class Node{
-        int value;
-        int weight;
-        Node(int value, int weight){
-            this.value = value;
-            this.weight = weight;
-        }
-    }
-    static class Vertex{
-        int value;
-        LinkedList<Node> edges;
-        Vertex(int value){
-            this.value = value;
-            edges = new LinkedList<>();
-        }
-        void addNode(Node node){
-            edges.add(node);
-        }
-    }
-
-    static class Graph{
-        ArrayList<Vertex> vertices;
-        int[] shortestPath;
-
-        public Graph(String filname, int num_vertices){
-            vertices = new ArrayList<>();
-            shortestPath = new int[num_vertices];
-            initGraph(filname);
-        }
-        private void initGraph(String filename){
-            try {
-                BufferedReader br = new BufferedReader(new FileReader(filename));
-                String line;
-                while((line=br.readLine())!=null){
-                    String[] items = line.trim().split("\t");
-                    int from = Integer.parseInt(items[0]);
-                    int to;
-                    int weight;
-                    Vertex vertex = new Vertex(from);
-                    for(int i=1;i<items.length;i++){
-                        String[] e = items[i].split(",");
-                        to = Integer.parseInt(e[0]);
-                        weight = Integer.parseInt(e[1]);
-                        vertex.addNode(new Node(to, weight));
-                    }
-                    vertices.add(vertex);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        public int[] Dijkstra(int startingVertex){
-            HashSet<Integer> X = new HashSet<>();
-            MinHeap heap = new MinHeap(shortestPath.length);
-            HashMap<Integer, Integer> labelToIndex = new HashMap<>();
-            HashMap<Integer, Integer> indexToLabel = new HashMap<>();
-            Vertex v = vertices.get(startingVertex-1);
-            for(Node node:v.edges){
-                int index = heap.insert(node.weight, labelToIndex, indexToLabel);
-                labelToIndex.put(node.value, index);
-                indexToLabel.put(index, node.value);
-            }
-            X.add(startingVertex);
-            for(int i=1;i<shortestPath.length;i++){
-                int cur = indexToLabel.get(0);
-                shortestPath[cur-1] = heap.extract_min(labelToIndex, indexToLabel);
-                X.add(cur);
-                v = vertices.get(cur-1);
-                for(Node node:v.edges){
-                    if(!X.contains(node.value)){
-                        int dijkstra_greedy_score = shortestPath[cur-1] + node.weight;
-                        int index;
-                        if(labelToIndex.containsKey(node.value)){
-                            int key = heap.delete(labelToIndex.get(node.value), labelToIndex, indexToLabel);
-                            index = heap.insert(Math.min(key, dijkstra_greedy_score), labelToIndex, indexToLabel);
-                        }else{
-                            index = heap.insert(dijkstra_greedy_score, labelToIndex, indexToLabel);
-                        }
-                        labelToIndex.put(node.value, index);
-                        indexToLabel.put(index, node.value);
-                    }
-                }
-            }
-            return shortestPath;
-        }
-    }
-
     public static void main(String[] args){
-        Graph graph = new Graph("dijkstraData.txt",200);
-        int[] targets = {7,37,59,82,99,115,133,165,188,197};
-        int[] shortestPath = graph.Dijkstra(1);
-        for(int i=0;i<targets.length;i++){
-            System.out.print(shortestPath[targets[i]-1]+",");
+        String filename = "Median.txt";
+        MaxHeap h_low = new MaxHeap(10000);
+        MinHeap h_high = new MinHeap(10000);
+        int i = 1;
+        long median_sum = 0;
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            String line;
+            while((line=br.readLine())!=null){
+                int val = Integer.parseInt(line);
+                if(i%2 != 0){
+                    // the number should be put into heap low
+                    if(!h_high.isEmpty() && val>h_high.get_min()){
+                        int tmp = h_high.extract_min();
+                        h_high.insert(val);
+                        h_low.insert(tmp);
+                    }else{
+                        h_low.insert(val);
+                    }
+                    i++;
+                }else{
+                    // the number should be put into heap high
+                    if(val<h_low.get_max()){
+                        int tmp = h_low.extract_max();
+                        h_low.insert(val);
+                        h_high.insert(tmp);
+                    }else{
+                        h_high.insert(val);
+                    }
+                    i++;
+                }
+                System.out.println((i-1)+": current median = " + h_low.get_max());
+                median_sum += h_low.get_max();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        System.out.println(median_sum%10000);
     }
 }
