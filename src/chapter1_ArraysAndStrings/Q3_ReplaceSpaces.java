afc98f30658da077efd266b47fddeead09d4fc60
chengyaoZhang <tc2002618@126.com>
2018-12-14 13:57:45 -0800

    Q1.3

---
+++ src/chapter1_ArraysAndStrings/Q3_ReplaceSpaces.java
@@ -0,0 +1,21 @@
+package chapter1_ArraysAndStrings;
+
+public class Q3_ReplaceSpaces {
+    void replaceSpaces(char[] str, int trueLength){
+        int spaceCnt = 0;
+        for(int i=0;i<trueLength;i++){
+            if(str[i]==' ') spaceCnt++;
+        }
+        int idx = trueLength + spaceCnt*2 - 1;
+        for(int i=trueLength-1;i>=0;i--){
+            if(str[i]==' '){
+                str[idx] = '0';
+                str[idx-1] = '2';
+                str[idx-2] = '%';
+                idx -= 3;
+            }else{
+                str[idx--] = str[i];
+            }
+        }
+    }
+}
