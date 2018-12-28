ace05060d6b5fff3e06c5a5b33f696c298e7932e
chengyaoZhang <tc2002618@126.com>
2018-12-28 10:55:21 -0800

    Q6.7

---
+++ src/chapter6_MathAndLogicalPuzzles/Q7_TheApocalypse.java
@@ -0,0 +1,25 @@
+package chapter6_MathAndLogicalPuzzles;
+
+import java.util.Random;
+
+public class Q7_TheApocalypse {
+    double getRatio(int n){
+        int boys = 0;
+        int girls = 0;
+        for(int i=0;i<n;i++){
+            int[] babies = runOneFamily();
+            boys += babies[0];
+            girls += babies[1];
+        }
+        return girls*1.0/boys;
+    }
+
+    int[] runOneFamily(){
+        int boys = 0;
+        Random rand = new Random();
+        while(rand.nextBoolean()){
+            boys++;
+        }
+        return new int[]{boys, 1};
+    }
+}
---
+++ src/chapter6_MathAndLogicalPuzzles/Test.java
@@ -0,0 +1,8 @@
+package chapter6_MathAndLogicalPuzzles;
+
+public class Test {
+    public static void main(String[] args){
+        Q7_TheApocalypse test = new Q7_TheApocalypse();
+        System.out.println(test.getRatio(100000));
+    }
+}
