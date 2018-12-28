6522bade78f5a83626f92b5141222e3cfc232168
chengyaoZhang <tc2002618@126.com>
2018-12-28 11:21:08 -0800

    Q6.8

---
+++ src/chapter6_MathAndLogicalPuzzles/Q8_TheEggDropProblem.java
@@ -0,0 +1,46 @@
+package chapter6_MathAndLogicalPuzzles;
+
+import java.util.Random;
+
+public class Q8_TheEggDropProblem {
+    private int numFloors;
+    private int breakingPoint;
+    private int countDrops;
+
+    Q8_TheEggDropProblem(){
+        numFloors = 100;
+        breakingPoint = new Random().nextInt(numFloors)+1;
+        countDrops = 0;
+        System.out.println("Answer: "+breakingPoint);
+    }
+
+    private boolean drop(int floor){
+        countDrops++;
+        return floor >= breakingPoint;
+    }
+
+    int findBreakingPoint(){
+        int interval = 14;
+        int previousFloor = 0;
+        int egg1 = interval;
+
+        while(egg1<=numFloors && !drop(egg1)){
+            interval--;
+            previousFloor = egg1;
+            egg1 += interval;
+        }
+
+        if(egg1>numFloors) return -1;
+
+        int egg2 = previousFloor + 1;
+        while(egg2<egg1 && !drop(egg2)){
+            egg2 += 1;
+        }
+
+        return egg2;
+    }
+
+    int getNumDrops(){
+        return countDrops;
+    }
+}
--- src/chapter6_MathAndLogicalPuzzles/Test.java
+++ src/chapter6_MathAndLogicalPuzzles/Test.java
@@ -1,8 +1,9 @@
 package chapter6_MathAndLogicalPuzzles;
 
-public class Test {
+public class Test {Q7_TheApocalypse test = new Q7_TheApocalypse();
     public static void main(String[] args){
-        Q7_TheApocalypse test = new Q7_TheApocalypse();
-        System.out.println(test.getRatio(100000));
+        Q8_TheEggDropProblem test = new Q8_TheEggDropProblem();
+        System.out.println(test.findBreakingPoint());
+        System.out.println(test.getNumDrops());
     }
 }
