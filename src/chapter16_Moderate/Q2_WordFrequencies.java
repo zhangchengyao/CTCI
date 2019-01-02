7ef05b374a16420f80256b5d1e3676e423a3dae7
chengyaoZhang <tc2002618@126.com>
2019-01-02 10:50:23 -0800

    Q16.2

---
+++ src/chapter16_Moderate/Q2_WordFrequencies.java
@@ -0,0 +1,21 @@
+package chapter16_Moderate;
+
+import java.util.HashMap;
+
+public class Q2_WordFrequencies {
+    HashMap<String, Integer> freq = new HashMap<>();
+
+    int getFrequent(String[] words, String word){
+        if(freq.isEmpty()) buildFreqTable(words);
+        return freq.get(word.toLowerCase());
+    }
+
+    private void buildFreqTable(String[] words){
+        for(String word: words){
+            word = word.toLowerCase();
+            if(word.trim().length()>0){
+                freq.put(word, freq.getOrDefault(word, 0)+1);
+            }
+        }
+    }
+}
