0235d853805015f33ffa95a2df79ba8ef06c36a2
chengyaoZhang <tc2002618@126.com>
2019-01-02 16:10:32 -0800

    Q16.8

--- src/chapter16_Moderate/Test.java
+++ src/chapter16_Moderate/Test.java
@@ -2,7 +2,7 @@ package chapter16_Moderate;
 
 public class Test {
     public static void main(String[] args){
-        Q7_NumberMax test = new Q7_NumberMax();
-        System.out.println(test.getMax(19,19));
+        Q8_EnglishInt test = new Q8_EnglishInt();
+        System.out.println(test.convert(2020));
     }
 }
