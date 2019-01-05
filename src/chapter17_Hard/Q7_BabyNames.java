0b2b34d3bd025a25a2ba4a077614dcaf28f6f206
chengyaoZhang <tc2002618@126.com>
2019-01-05 11:07:26 -0800

    Q17.7

---
+++ src/chapter17_Hard/Q7_BabyNames.java
@@ -0,0 +1,42 @@
+package chapter17_Hard;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Q7_BabyNames {
+    HashMap<String, Integer> realNameFreq(HashMap<String, Integer> rawData, List<String[]> synonyms){
+        HashMap<String, String> parent = new HashMap<>();
+        for(String key: rawData.keySet()){
+            parent.put(key, key);
+        }
+        for(String[] synonym: synonyms){
+            String name1 = synonym[0];
+            String name2 = synonym[1];
+            if(rawData.containsKey(name1) && rawData.containsKey(name2))
+                union(parent, name1, name2);
+        }
+
+        HashMap<String, Integer> res = new HashMap<>();
+        for(String key: rawData.keySet()){
+            String realName = find(parent, key);
+            res.putIfAbsent(realName, 0);
+            res.put(realName, res.get(realName)+rawData.get(key));
+        }
+
+        return res;
+    }
+
+    private String find(HashMap<String, String> parent, String name){
+        while(!name.equals(parent.get(name))){
+            name = parent.get(name);
+        }
+        return name;
+    }
+
+    private void union(HashMap<String, String> parent, String name1, String name2){
+        String root1 = find(parent, name1);
+        String root2 = find(parent, name2);
+        if(root1.equals(root2)) return ;
+        parent.put(root2, root1);
+    }
+}
--- src/chapter17_Hard/Test.java
+++ src/chapter17_Hard/Test.java
@@ -1,8 +1,26 @@
 package chapter17_Hard;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Test {
     public static void main(String[] args){
-        System.out.println(new Q6_CountOf2s().num2s(61523));
-        System.out.println(new Q6_CountOf2s().count2sInRange(61523));
+        HashMap<String, Integer> raw = new HashMap<>();
+        raw.put("John", 15);
+        raw.put("Jon", 12);
+        raw.put("Chris", 13);
+        raw.put("Kris", 4);
+        raw.put("Christopher", 19);
+        List<String[]> syn = new ArrayList<>();
+        syn.add(new String[]{"Jon", "John"});
+        syn.add(new String[]{"John", "Johnny"});
+        syn.add(new String[]{"Chris", "Kris"});
+        syn.add(new String[]{"Chris", "Christopher"});
+        HashMap<String, Integer> res = new Q7_BabyNames().realNameFreq(raw, syn);
+        for(Map.Entry<String, Integer> entry: res.entrySet()){
+            System.out.println(entry.getKey()+" "+entry.getValue());
+        }
     }
 }
